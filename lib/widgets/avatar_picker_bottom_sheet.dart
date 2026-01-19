1bdc0b4682586de12109a873e34f229cddb050e5
frozensum <frozensum@tuta.io>
2026-01-19 20:36:00 +0530

    Added more avatars

--- lib/widgets/avatar_picker_bottom_sheet.dart
+++ lib/widgets/avatar_picker_bottom_sheet.dart
@@ -30,57 +30,133 @@ class _AvatarPickerBottomSheetState extends State<AvatarPickerBottomSheet> {
   // Categorized avatars by anime
   final List<AnimeCategory> animeCategories = [
     AnimeCategory(
-      title: "Frieren: Beyond Journey's End",
+      title: "Default",
       avatars: [
-        AvatarItem(path: 'assets/avatars/avatar1.jpg'),
-        AvatarItem(path: 'assets/avatars/avatar2.jpg'),
-        AvatarItem(path: 'assets/avatars/avatar3.jpg', isAvailable: false),
-        AvatarItem(path: 'assets/avatars/avatar4.jpg', isAvailable: false),
-        AvatarItem(path: 'assets/avatars/avatar5.jpg', isAvailable: false),
+        AvatarItem(path: 'assets/avatars/Default/avatar1.jpg'),
+        AvatarItem(path: 'assets/avatars/Default/avatar2.jpg'),
+        AvatarItem(path: 'assets/avatars/Default/avatar3.jpg'),
+        AvatarItem(path: 'assets/avatars/Default/avatar4.jpg'),
+        AvatarItem(path: 'assets/avatars/Default/avatar5.jpg'),
+        AvatarItem(path: 'assets/avatars/Default/avatar6.jpg'),
+        AvatarItem(path: 'assets/avatars/Default/avatar7.jpg'),
       ],
     ),
-    AnimeCategory(
-      title: "Jujutsu Kaisen",
-      avatars: [
-        AvatarItem(path: 'assets/avatars/avatar6.jpg'),
-        AvatarItem(path: 'assets/avatars/avatar7.jpg'),
-        AvatarItem(path: 'assets/avatars/avatar1.jpg', isAvailable: false),
-        AvatarItem(path: 'assets/avatars/avatar2.jpg', isAvailable: false),
-        AvatarItem(path: 'assets/avatars/avatar3.jpg', isAvailable: false),
-        AvatarItem(path: 'assets/avatars/avatar4.jpg', isAvailable: false),
-      ],
-    ),
+    // Attack on Titan
     AnimeCategory(
       title: "Attack on Titan",
       avatars: [
-        AvatarItem(path: 'assets/avatars/avatar4.jpg', isAvailable: false),
-        AvatarItem(path: 'assets/avatars/avatar5.jpg', isAvailable: false),
-        AvatarItem(path: 'assets/avatars/avatar6.jpg', isAvailable: false),
-        AvatarItem(path: 'assets/avatars/avatar7.jpg', isAvailable: false),
-        AvatarItem(path: 'assets/avatars/avatar1.jpg', isAvailable: false),
+        AvatarItem(path: 'assets/avatars/AOT/aot_fc_eren-avatar.png'),
+        AvatarItem(path: 'assets/avatars/AOT/aot_fc_mikasa-avatar.png'),
+        AvatarItem(path: 'assets/avatars/AOT/aot_fc_levi-avatar.png'),
+        AvatarItem(path: 'assets/avatars/AOT/aot_fc_armin-avatar.png'),
+        AvatarItem(path: 'assets/avatars/AOT/aot_fc_annie-avatar.png'),
+        AvatarItem(path: 'assets/avatars/AOT/aot_fc_hange-avatar.png'),
+        AvatarItem(path: 'assets/avatars/AOT/aot_fc_jean-avatar.png'),
+        AvatarItem(path: 'assets/avatars/AOT/aot_fc_reiner-avatar.png'),
+        AvatarItem(path: 'assets/avatars/AOT/aot_fc_sasha-avatar.png'),
+        AvatarItem(path: 'assets/avatars/AOT/aot_fc_conny-avatar.png'),
       ],
     ),
+    // Jujutsu Kaisen
     AnimeCategory(
-      title: "Demon Slayer",
+      title: "Jujutsu Kaisen",
       avatars: [
-        AvatarItem(path: 'assets/avatars/avatar5.jpg', isAvailable: false),
-        AvatarItem(path: 'assets/avatars/avatar6.jpg', isAvailable: false),
-        AvatarItem(path: 'assets/avatars/avatar7.jpg', isAvailable: false),
-        AvatarItem(path: 'assets/avatars/avatar1.jpg', isAvailable: false),
-        AvatarItem(path: 'assets/avatars/avatar2.jpg', isAvailable: false),
-        AvatarItem(path: 'assets/avatars/avatar3.jpg', isAvailable: false),
+        AvatarItem(
+          path: 'assets/avatars/jjk/1044-jujutsu-kaisen-satoru-gojo.png',
+        ),
+        AvatarItem(
+          path: 'assets/avatars/jjk/1041-jujutsu-kaisen-yuji-itadori.png',
+        ),
+        AvatarItem(
+          path: 'assets/avatars/jjk/1042-jujutsu-kaisen-megumi-fushigoro.png',
+        ),
+        AvatarItem(
+          path: 'assets/avatars/jjk/1043-jujutsu-kaisen-nobara-kugisaki.png',
+        ),
+        AvatarItem(
+          path: 'assets/avatars/jjk/1045-jujutsu-kaisen-ryomen-sukuna.png',
+        ),
       ],
     ),
+    // Solo Leveling
+    AnimeCategory(
+      title: "Solo Leveling",
+      avatars: [
+        AvatarItem(path: 'assets/avatars/solo leveling/solo_sungjinwoo.png'),
+        AvatarItem(path: 'assets/avatars/solo leveling/solo_chahaein.png'),
+        AvatarItem(path: 'assets/avatars/solo leveling/solo_yoojinho.png'),
+        AvatarItem(path: 'assets/avatars/solo leveling/solo_baekyoonho.png'),
+        AvatarItem(path: 'assets/avatars/solo leveling/solo_choijongin.png'),
+        AvatarItem(path: 'assets/avatars/solo leveling/solo_teaser_visual.png'),
+      ],
+    ),
+    // Re:Zero
+    AnimeCategory(
+      title: "Re:Zero",
+      avatars: [
+        AvatarItem(path: 'assets/avatars/reZero/rezero_s3_avatar_01.jpg'),
+        AvatarItem(path: 'assets/avatars/reZero/rezero_s3_avatar_02.jpg'),
+        AvatarItem(path: 'assets/avatars/reZero/rezero_s3_avatar_04.jpg'),
+        AvatarItem(path: 'assets/avatars/reZero/rezero_s3_avatar_05.jpg'),
+        AvatarItem(path: 'assets/avatars/reZero/rezero_s3_avatar_06.jpg'),
+        AvatarItem(path: 'assets/avatars/reZero/rezero_s3_avatar_07.jpg'),
+        AvatarItem(path: 'assets/avatars/reZero/rezero_s3_avatar_09.jpg'),
+        AvatarItem(path: 'assets/avatars/reZero/rezero_s3_avatar_11.jpg'),
+        AvatarItem(path: 'assets/avatars/reZero/rezero_s3_avatar_12.jpg'),
+        AvatarItem(path: 'assets/avatars/reZero/rezero_s3_avatar_13.jpg'),
+        AvatarItem(path: 'assets/avatars/reZero/rezero_s3_avatar_14.jpg'),
+        AvatarItem(path: 'assets/avatars/reZero/rezero_s3_avatar_17.jpg'),
+        AvatarItem(path: 'assets/avatars/reZero/rezero_s3_avatar_18.jpg'),
+        AvatarItem(path: 'assets/avatars/reZero/rezero_s3_avatar_19.jpg'),
+        AvatarItem(path: 'assets/avatars/reZero/rezero_s3_avatar_20.jpg'),
+        AvatarItem(path: 'assets/avatars/reZero/rezero_s3_avatar_21.jpg'),
+      ],
+    ),
+    // Spy x Family
     AnimeCategory(
       title: "Spy x Family",
       avatars: [
-        AvatarItem(path: 'assets/avatars/avatar3.jpg', isAvailable: false),
-        AvatarItem(path: 'assets/avatars/avatar4.jpg', isAvailable: false),
-        AvatarItem(path: 'assets/avatars/avatar5.jpg', isAvailable: false),
-        AvatarItem(path: 'assets/avatars/avatar6.jpg', isAvailable: false),
-        AvatarItem(path: 'assets/avatars/avatar7.jpg', isAvailable: false),
+        AvatarItem(
+          path: 'assets/avatars/spyXfamily/100006-spy-x-family-loid.png',
+        ),
+        AvatarItem(
+          path: 'assets/avatars/spyXfamily/100007-spy-x-family-yor.png',
+        ),
+        AvatarItem(
+          path: 'assets/avatars/spyXfamily/100008-spy-x-family-anya-1.png',
+        ),
       ],
     ),
+    // The Apothecary Diaries
+    AnimeCategory(
+      title: "The Apothecary Diaries",
+      avatars: [
+        AvatarItem(path: 'assets/avatars/apothecary/apothecary_maomao-1.png'),
+        AvatarItem(path: 'assets/avatars/apothecary/apothecary_jinshi-2.png'),
+        AvatarItem(path: 'assets/avatars/apothecary/apothecary_gyokuyou.png'),
+        AvatarItem(path: 'assets/avatars/apothecary/apothecary_gaoshun.png'),
+        AvatarItem(path: 'assets/avatars/apothecary/apothecary_lihaku.png'),
+        AvatarItem(path: 'assets/avatars/apothecary/apothecary_xiaolan.png'),
+      ],
+    ),
+    // Bocchi The Rock
+    AnimeCategory(
+      title: "Bocchi The Rock!",
+      avatars: [
+        AvatarItem(path: 'assets/avatars/bochitherock/bocchi-avatar.jpg'),
+        AvatarItem(path: 'assets/avatars/bochitherock/bocchi-kita-avatar.jpg'),
+        AvatarItem(path: 'assets/avatars/bochitherock/bocchi-ryo-avatar.jpg'),
+        AvatarItem(
+          path: 'assets/avatars/bochitherock/bochhi-nijika-avatar.jpg',
+        ),
+        AvatarItem(
+          path: 'assets/avatars/bochitherock/bocchi-nijika-kv-avatar.jpg',
+        ),
+        AvatarItem(path: 'assets/avatars/bochitherock/bocchi-kv-avatar.jpg'),
+      ],
+    ),
+
+    // Default Avatars
   ];
 
   String? selectedAvatar;
